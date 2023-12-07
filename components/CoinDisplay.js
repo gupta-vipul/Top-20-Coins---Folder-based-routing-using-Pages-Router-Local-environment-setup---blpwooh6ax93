f5eeb2128297913d2d9f47ee472b5a9ea298a8bc
Vipul Gupta <mail.vipulg@gmail.com>
2023-12-07 18:21:57 +0530

    required code

--- components/CoinDisplay.js
+++ components/CoinDisplay.js
@@ -1,16 +1,33 @@
 import React, { useState, useEffect } from "react";
 import CoinCard from "./CoinCard";
 
-function CoinDisplay({ coin }) {
+function CoinDisplay({coin}) {
   const [coins, setCoins] = useState([]);
+
+  async function getCoins() {
+    const response = await fetch("https://api.coinlore.net/api/tickers/");
+    const data = await response.json();
+    setCoins(data.data);
+    // console.log(data.data);
+  }
+
+  useEffect(()=>{
+    getCoins();
+  },[])
 
   return (
     <div className="home">
       <h1>Top 20 Cryptos</h1>
       <div className="coins-container">
-        {coins.map((coin) => (
-          <CoinCard />
-        ))}
+        {
+          coins.map((coin, index)=>{
+            if(index < 20) {
+              return (
+                <CoinCard coin={coin} key={coin.id}/>
+              )
+            }
+          })
+        }
       </div>
     </div>
   );
--- pages/coins/[coin_id].js
+++ pages/coins/[coin_id].js
@@ -1,20 +1,32 @@
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 
 function CoinDetail() {
   const [coin, setCoin] = useState(null);
+  const {query} = useRouter();
+  // console.log(query);
 
+  async function getCoinDetails(id) {
+    const response = await fetch(`https://api.coinlore.net/api/ticker/?id=${id}`);
+    const data = await response.json();
+    setCoin(data[0]);
+    // console.log(data);
+  }
+  useEffect(()=>{
+    getCoinDetails(query.coin_id); 
+  },[]);
   return (
     <div className="coin-detail">
       <div className="name-symbol">
-        <h1 className="name">coin-name</h1>
-        <h2 className="symbol">(coin-symbol)</h2>
+        <h1 className="name">{coin?.name}</h1>
+        <h2 className="symbol">({coin?.symbol})</h2>
       </div>
       <div className="market-description">
-        <p className="coin-rank">Rank: coin-symbol</p>
-        <p className="coin-price">Price: $coin-price</p>
-        <p className="coin-market-cap">Market Cap: $coin-market_cap_usd</p>
-        <p className="coin-total-supply">Total Supply: coin-tsupply</p>
-        <p className="coin-max-supply">Max Supply: coin-msupply</p>
+        <p className="coin-rank">Rank: {coin?.rank}</p>
+        <p className="coin-price">Price: ${coin?.price_usd}</p>
+        <p className="coin-market-cap">Market Cap: ${coin?.market_cap_usd}</p>
+        <p className="coin-total-supply">Total Supply: {coin?.tsupply}</p>
+        <p className="coin-max-supply">Max Supply: {coin?.msupply ? coin.msupply : "N/A"}</p>
       </div>
     </div>
   );
